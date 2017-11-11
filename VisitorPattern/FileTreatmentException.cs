c6a0405dfc77bcbc3eab1a88a083813f6c902608
Aya <Aya@AYA-PC>
2017-11-11 09:42:38 +0900

    first commit

---
+++ VisitorPattern/FileTreatmentException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+    public class FileTreatmentException : Exception
+    {
+        public FileTreatmentException()
+        {
+
+        }
+
+        public FileTreatmentException(string msg) : base(msg)
+        {
+            
+        }
+        
+    }
+}
---
+++ VisitorPattern/ListVisitor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+    public class ListVisitor :Visitor
+    {
+        private String currentdir = "";
+
+        public override void Visit(File file)
+        {
+            Console.WriteLine(currentdir + "/" + file);
+        }
+
+        public override void Visit (Directory directory)
+        {
+            Console.WriteLine(currentdir + "/" + directory);
+            String savedir = currentdir;
+            currentdir = currentdir + "/" + directory.GetName();
+            IEnumerator it = directory.GetEnumerator();
+
+            while (it.MoveNext())
+            {
+                Entry entry = (Entry)it.Current;
+                entry.Accept(this);
+            }
+            currentdir = savedir;
+        }
+    }
+}
